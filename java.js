752ed2ba5a77fe0517a3bc1735c8c63e208d8d70
Jan <cubic.printing@seznam.cz>
2024-04-22 20:38:23 +0200

    sa

--- java.js
+++ java.js
@@ -2,6 +2,6 @@ let nevim = 1;
 document.querySelector("p").innerHTML = nevim;
 
 let press = () => {
-    nevim = nevim + 1;
+    nevim = nevim += 1;
     document.querySelector("p").innerHTML = nevim;
 }
